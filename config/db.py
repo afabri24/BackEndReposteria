ac3cea321f17c18562983308cdb2783dc7fae992
Angel Fabrizio Franyutti Pulido <afabri24@gmail.com>
2024-04-16 10:34:13 -0600

    Configuracion de base de datos remota

--- config/db.py
+++ config/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
+# engine = create_engine('mysql+pymysql://sql5699343:TUBEuTZEl6@sql5.freesqldatabase.com:3306/sql5699343')
 engine = create_engine('mysql+pymysql://adminReposteria:123456789@127.0.0.1:3306/reposteriarosario')
 
 meta_data = MetaData()
