bba9274d3fe63f8cd22691cfb0228a8eaec68ff4
Angel Fabrizio Franyutti Pulido <afabri24@gmail.com>
2024-04-17 18:59:40 -0600

    Cambie router

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from router.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",  # Asegúrate de cambiar esto a la URL de tu aplicación
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(api_router)
+
