e8ce44f4f15a1b2343ab1646526f3da9194029fb
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-05-06 14:43:37 -0600

    Actualizacion
    nueva

--- schema/usuario_schema.py
+++ schema/usuario_schema.py
@@ -12,3 +12,23 @@ class UsuarioSchema(BaseModel):
 class LoginSchema(BaseModel):
     email: str
     password: str
+
+class ActualizarPerfilSchema(BaseModel):
+    email: str
+    nombre: Optional[str]
+    apellido: Optional[str]
+    password: Optional[str]
+
+class ActualizarPerfilSchema(BaseModel):
+    email_viejo: str
+    nuevo_email: Optional[str] = None
+    nombre: Optional[str] = None
+    apellido: Optional[str] = None
+
+class ActualizarContrasenaSchema(BaseModel):
+    email: str
+    password: str
+
+class RegistrarUsuarioSchema(BaseModel):
+    email: str
+    password: str
